1b2d3de702db53a1c7fdca8c23d96421a5e4aa1f
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-24 17:58:37 +0200

    feat: Add operate() function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 console.log('Starting Javascript');
 
+let num1;
+let num2;
+let operator;
+
 // calculator functions
 
 function addNumbers(num1, num2) {
@@ -16,4 +20,19 @@ function multiplyNumbers(num1, num2) {
 
 function divideNumbers(num1, num2) {
     return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case 'add': 
+            return addNumbers(num1, num2); 
+        case 'subtract': 
+            return subtractNumbers(num1, num2); 
+        case 'multiply': 
+            return multiplyNumbers(num1, num2); 
+        case 'divide': 
+            return divideNumbers(num1, num2); 
+        default:
+            console.log('no valid operator found');
+    }
 }
