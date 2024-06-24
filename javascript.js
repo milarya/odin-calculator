d94861a551e6e0a5521716117425495f75c21029
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-24 17:49:49 +0200

    feat: Add basic calculator functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 console.log('Starting Javascript');
+
+// calculator functions
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+function subtractNumbers(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+function divideNumbers(num1, num2) {
+    return num1 / num2;
+}
