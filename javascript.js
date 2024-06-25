7b4aa101acccb8a76ceffbf7068aea38d310c546
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-25 18:50:55 +0200

    feat: Change styling for mobile viewports

--- javascript.js
+++ javascript.js
@@ -36,3 +36,27 @@ function operate(operator, num1, num2) {
             console.log('no valid operator found');
     }
 }
+
+/* @todo implement on/off funcionality later!
+const btnOn = document.querySelector('.on');
+const btnOff = document.querySelector('.off');
+
+btnOn.addEventListener('click', () => {
+    toggleCalculatorState(true);
+});
+
+btnOff.addEventListener('click', () => {
+    toggleCalculatorState(false);
+});
+
+function toggleCalculatorState(bool) {
+    if (bool === true) {
+        clearResult();
+        addResult('0,0');
+    } else {
+        // empty results
+        clearResult();
+        // make all buttons except 'on' disabled
+    }
+}
+*/
